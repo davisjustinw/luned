24b21cf80d3fad0e352724555e4f1157800b0525
Justin Davis <davisjustinw@gmail.com>
2019-02-04 05:53:32 +0000

    add color squares

--- lib/weather911/cli.rb
+++ lib/weather911/cli.rb
@@ -3,27 +3,35 @@
 class Weather911::CLI
 
   def start
-    puts "Follow, me... \u{1F311} \u{1F312} \u{1F313} \u{1F314} \u{1F315} \u{1F316} \u{1F317} \u{1F318}"
-    top_menu
+    puts ''
+    puts "    Follow, me...\u{1f407}"
+    puts "\u{1F311} \u{1F312} \u{1F313} \u{1F314} \u{1F315} \u{1F316} \u{1F317} \u{1F318} \u{1F311}"
+    puts ''
+    prompt_month
     display_month('jan', '2019')
-    #month_menu
+    prompt_day
     #Weather911::API.get_data(ARGV[0])
   end
 
-  def top_menu
-    puts "Enter: <mon> <yyyy> or <quit>"
-
+  def prompt_month
+    print "<mon> <yyyy>: "
+    input = gets.chomp
   end
 
   def display_month(month, year)
-    puts ''
     puts "| Su | Mo | Tu | We | Th | Fr | Sa |"
     puts "|    |    |  1 |  2 |  3 |  4 |  5 |"
-    puts "|  6 |  7 |  8 |  9 | 10 | 11 | 12 |"
+    puts "|  6 |  7 | #{red(' 8')} |  9 | 10 | 11 | 12 |"
     puts "| 13 | 14 | 15 | 16 | 17 | 18 | 19 |"
-    puts "| 20 | 21 | 22 | 23 | 24 | 25 | 26 |"
+    puts "| 20 | 21 | 22 | #{green('23')} | 24 | 25 | 26 |"
     puts "| 27 | 28 | 29 | 30 | 21 |    |    |"
-    #c = Weather911::Color.new
-    binding.pry
+    puts ''
   end
+
+  def prompt_day
+    print "<day>: "
+    input = gets.chomp
+  end
+
+  
 end
--- lib/weather911/color.rb
+++ lib/weather911/color.rb
@@ -1,9 +1,10 @@
 #class Weather911::Color
   def colorize(text, color_code)
-    "\e[1;#{color_code}m#{text}\e[0m"
+    "\e[30;#{color_code}m#{text}\e[0m"
   end
-  def red(text); colorize(text, "31"); end
-  def yellow(text); colorize(text, "33"); end
-  def green(text); colorize(text, "32"); end
+
+  def red(text); colorize(text, "101"); end
+  def yellow(text); colorize(text, "103"); end
+  def green(text); colorize(text, "102"); end
 
 #end
