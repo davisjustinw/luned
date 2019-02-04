4787ce3140ee773ed182c1f3cc2a88da12386eaf
Justin Davis <davisjustinw@gmail.com>
2019-02-04 04:25:41 +0000

    wip display_month

--- lib/weather911/cli.rb
+++ lib/weather911/cli.rb
@@ -11,10 +11,15 @@ class Weather911::CLI
 
   def top_menu
     puts "Enter: <mon> <yyyy> or <quit>"
-    
+
   end
 
   def display_month(month, year)
-
+    puts "| Su | Mo | Tu | We | Th | Fr | Sa |"
+    puts "|    |    |  1 |  2 |  3 |  4 |  5 |"
+    puts "|  6 |  7 |  8 |  9 | 10 | 11 | 12 |"
+    puts "| 13 | 14 | 15 | 16 | 17 | 18 | 19 |"
+    puts "| 20 | 21 | 22 | 23 | 24 | 25 | 26 |"
+    puts "| 27 | 28 | 29 | 30 | 21 |    |    |"
   end
 end
