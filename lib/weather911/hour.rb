47cbbc9624fc8ea81512d24aa2c55f39cc5f2d1f
davisjustinw <davisjustinw@gmail.com>
2019-02-23 15:26:10 -0800

    add hour

--- lib/weather911/hour.rb
+++ lib/weather911/hour.rb
@@ -1,7 +1,9 @@
 class Weather911::Hour
   attr_accessor :incidents, :observed
+  attr_reader :is
 
-  def initialize
+  def initialize(hour)
+    @is = hour 
     @incidents = []
   end
 
@@ -12,5 +14,5 @@ class Weather911::Hour
   def create_incident(type, address)
     add_incident(Weather911::Incident.new(type, address))
   end
-  
+
 end
