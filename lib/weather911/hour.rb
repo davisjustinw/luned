769977de993689a1767ee92e3196fc3fa37cb854
davisjustinw <davisjustinw@gmail.com>
2019-02-23 17:11:04 -0800

    add hour all

--- lib/weather911/hour.rb
+++ lib/weather911/hour.rb
@@ -1,9 +1,16 @@
 class Weather911::Hour
   attr_accessor :incidents, :observed, :day
 
+  @@all = []
+
   def initialize(year, month, day, hour)
     @datetime = DateTime.new(year, month, day, hour)
     @incidents = []
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 
   def is
