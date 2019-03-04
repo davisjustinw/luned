529bf46e439861c35186495449847e5037e78b04
davisjustinw <davisjustinw@gmail.com>
2019-03-03 16:33:02 -0800

    moved greeting and credits to View

--- lib/luned/controller.rb
+++ lib/luned/controller.rb
@@ -1,32 +1,19 @@
-#controller
+## Entry point for Luned command line interface.
+#Initializes with the
 
 class Luned::Controller
-  #@@PROMPTS = ["<yyyy>", "<mm>", "<dd>", "<hr24>"]
-  attr_accessor :prompt, :view, :api
+
+  attr_accessor :prompt, :view
 
   def initialize
     @prompt = Luned::Prompt.new
     @view = Luned::View.new
-    @api = Luned::API.new
   end
 
   def start
-
-    print "\n\n==============  Luned  =============="
-    print "\n\n     \u{1F311} \u{1F312} \u{1F313} \u{1F314} \u{1F315} \u{1F316} \u{1F317} \u{1F318} \u{1F311}"
-    print "\n\n======  Seattle 911 + Weather  ======\n\n"
-    print " - 01 July 2010 to Present\n"
-    print " - <..> move up one layer\n"
-    print " - <q> quit.\n"
-    print " - Examples:\n   - 2010 7\n   - 2010 7 4\n\n"
-
-
+    view.greeting
     control_prompt
-    print "\n======================================\n\n"
-    print " - Powered by Dark Sky:\n    https://darksky.net/poweredby/\n\n"
-    print " - Socrata & Seattle 911:\n    https://dev.socrata.com/foundry/data.seattle.gov/grwu-wqtk\n\n"
-    print " - Justin Davis, 2019:\n    https://github.com/davisjustinw/luned\n\n"
-
+    view.credits
   end
 
   def prompt
--- lib/luned/view.rb
+++ lib/luned/view.rb
@@ -1,5 +1,22 @@
 class Luned::View
 
+  def greeting
+    print "\n\n==============  Luned  =============="
+    print "\n\n     \u{1F311} \u{1F312} \u{1F313} \u{1F314} \u{1F315} \u{1F316} \u{1F317} \u{1F318} \u{1F311}"
+    print "\n\n======  Seattle 911 + Weather  ======\n\n"
+    print " - 01 July 2010 to Present\n"
+    print " - <..> move up one layer\n"
+    print " - <q> quit.\n"
+    print " - Examples:\n   - 2010 7\n   - 2010 7 4\n\n"
+  end
+
+  def credits
+    print "\n======================================\n\n"
+    print " - Powered by Dark Sky:\n    https://darksky.net/poweredby/\n\n"
+    print " - Socrata & Seattle 911:\n    https://dev.socrata.com/foundry/data.seattle.gov/grwu-wqtk\n\n"
+    print " - Justin Davis, 2019:\n    https://github.com/davisjustinw/luned\n\n"
+  end
+
   def display(args)
     case args.size
     when 2
