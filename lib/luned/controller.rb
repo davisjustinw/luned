d5a92733754bdff18f8ee6502a05bc01a344ea32
davisjustinw <davisjustinw@gmail.com>
2019-03-03 18:42:35 -0800

    commented and streamlined Controller

--- lib/luned/controller.rb
+++ lib/luned/controller.rb
@@ -1,9 +1,10 @@
 ## Entry point for Luned command line interface.
-#Initializes with the
+# Prompt controls input and breadcrumb.
+# View displays data and triggers obect creation with API calls when needed.
 
 class Luned::Controller
 
-  attr_accessor :prompt, :view
+  attr_reader :prompt, :view
 
   def initialize
     @prompt = Luned::Prompt.new
@@ -11,33 +12,24 @@ class Luned::Controller
   end
 
   def start
+    ## Entry point
     view.greeting
     control_prompt
     view.credits
   end
 
-  def prompt
-    @prompt
-  end
-
-  def breadcrumb
-    prompt.breadcrumb
-  end
-
-  def view
-    @view
-  end
-
   def control_prompt
+    ## Central business logic.
+    # pop_hour allows calling repeated hours without ..
     while !prompt.quit? do
       prompt.display_breadcrumb
       prompt.get_args
       if !prompt.quit? && prompt.valid_args?
         prompt.submit_args
-        view.display(breadcrumb)
+        view.display(prompt.breadcrumb)
         prompt.pop_hour
       end
     end
   end
-
+  
 end
